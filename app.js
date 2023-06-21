4cb6daa808a9349084b7347d81cc625351be4c44
Md Shoaib Raza <razashoaib270@gmail.com>
2023-06-21 23:17:28 +0530

    task app completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const uuid = require('uuid')
 
-const { getAllTasks, addNewTask } = require("./utils/task");
+const { getAllTasks, addNewTask, getTaskById, deleteTaskById, updateTaskByid } = require("./utils/task");
 
 const app = express();
 app.use(express.json());
@@ -13,13 +14,45 @@ app.get("/tasks", (req,res) => {
 
 app.post("/tasks",(req,res) => {
   let task = {...req.body};
-  addNewTask(task);
+  task = addNewTask(task);
   return res.status(201).send({
     message: "Task added successfully",
     task,
   })
+});
+
+app.get("/tasks/:taskId",(req,res) => {
+  let taskId = req.params.taskId;
+  let task = getTaskById(taskId);
+  if(!task){
+    return res.status(404).send({
+      error:`No such Task found with id { ${taskId} }`
+    })
+    }else{
+      return res.status(200).send(task)}
 })
 
+app.delete("/delete/:taskId",(req, res) => {
+  let taskId = req.params.taskId
+  let isDeleted = deleteTaskById(taskId)
+  if (!isDeleted ) {
+    return res.status(500).send("Error in deleting the task")
+    } else {
+      return res.status(200).send({
+        message:"The task has been deleted succesfully!"
+        })}
+})
+
+app.put("/tasks", (req,res) => {
+  let taskBody = req.body;
+  let isUpdated = updateTaskByid(taskBody)
+  if (!isUpdated ){
+    return res.status(500).send('error updating tasks')
+    }
+    else {
+      return res.status(201).json(taskBody);
+      }
+});
 app.listen(PORT, () =>{
   console.log(`Server is running on port ${PORT}`);
 });
--- utils/task.js
+++ utils/task.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
+const uuid = require("uuid");
 
 const TASK_FILE = "task.json";
+const EDITABLE_ITEMS = ["body", "title"]
 
 let getAllTasks = () =>{
   try{
@@ -14,11 +16,47 @@ let getAllTasks = () =>{
 
 let addNewTask = (task) => {
   let allTasks = getAllTasks();
+  task = {taskId : uuid.v4(), ...task}
   allTasks.push(task);
   fs.writeFileSync(TASK_FILE, JSON.stringify(allTasks))
+  return task;
+};
+
+let getTaskById = (taskId) => {
+  let tasks = getAllTasks();
+  let task = tasks.find((task) => task.taskId === taskId);
+  return task;
+}
+
+let deleteTaskById = (taskId) => {
+  let tasks = getAllTasks()
+  let originalLength = tasks.length
+  tasks = tasks.filter((task) => task.taskId !== taskId)
+  //extra
+  fs.writeFileSync(TASK_FILE,JSON.stringify(tasks))
+  return tasks.length < originalLength
+}
+
+let updateTaskByid = (task) => {
+  let {taskId} = task
+  let tasks = getAllTasks()
+  let isUpdated = false
+  for(let t of tasks){
+    if(t.taskId === taskId){
+      for(let field of EDITABLE_ITEMS){
+        t[field] = task[field]
+      }
+      isUpdated = true
+    }
+  }
+  fs.writeFileSync(TASK_FILE, JSON.stringify(tasks))
+  return isUpdated
 };
 
 module.exports = {
   getAllTasks,
-  addNewTask
+  addNewTask,
+  getTaskById,
+  deleteTaskById,
+  updateTaskByid,
 }
